d996908a51913f2046c020f8f48c348e655d48e4
sathyadevops <sathyadevops1@gmail.com>
2020-12-23 02:11:49 +0000

    c-code

---
+++ sample.c
@@ -0,0 +1,10 @@
+void main()
+{
+  printf(" Test code");
+  printf(" Test code");
+  printf(" Test code");
+  printf(" Test code");
+  printf(" Test code");
+  printf(" Test code");
+  printf(" Test code");
+}
